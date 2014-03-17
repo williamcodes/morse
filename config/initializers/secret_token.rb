81ef602a5932470c3ee7d9ddba68aae1fbd790de
wfjeff <wm.jeffries@gmail.com>
2014-03-17 09:18:20 -0400

    initialize rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Morse::Application.config.secret_key_base = '3f7ab4afe011594fbc58d683f47094d9264113b1951eae98e61fd6d7c6365f64ac098254f074d4de326e7801c62d92ecb2c35223e49052a6915c4207b143842a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Morse::Application.config.session_store :cookie_store, key: '_morse_session'
